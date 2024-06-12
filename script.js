9f807264b292411dd40a4f78c9693ff3e3ed2a61
Ethan <etheaarygame@gmail.com>
2024-06-12 19:39:39 +1200

    fix subtitle

--- script.js
+++ script.js
@@ -21,134 +21,145 @@ function getLetterFromNumber(r,t=!0){const e=t?65:97;let o="";do{o=String.fromCh
 const fetchConfig = fetch('config.json');
 const fetchText = fetch('consoleanimationtext.txt');
 
-document.addEventListener('keydown', async function start(event) {
-    if (event.key === 'Enter') {
-        document.removeEventListener('keydown', start);
+document.addEventListener("DOMContentLoaded", async () => {
+    section_number = 0;
+    question_number = 0;
 
-        // fadeOutElements
-        document.querySelector('.title').classList.add('fade-out');
-        document.querySelector('.subtitle').classList.add('fade-out');
-        document.querySelector('.begin-text').classList.add('fade-out');
-        await pause(2100);
-        document.getElementById("console").style.display = 'block';
-
-        // fetchTextFromFile
-        let textArray = [];
-        try {
-            const response = await fetchText; // await fetch('consoleanimationtext.txt');
-            if (!response.ok) {
-                throw new Error('Failed to fetch text');
-            }
-            const text = await response.text();
-            textArray = text.split('\n');
-        } catch (error) {
-            console.error('Error fetching text:', error);
-        }
-
-        // displayConsoleOutput
-        const delay = 10;
-        const consoleElement = document.getElementById('console');
-        let lines = [];
-
-        const viewportHeight = window.innerHeight;
-        const paddingTopBottom = 40 * 2;
-        const fontSize = parseFloat(getComputedStyle(consoleElement).fontSize);
-        const lineHeight = fontSize * 1.2;
-        const availableHeight = viewportHeight - paddingTopBottom;
-        const maxLines = Math.floor(availableHeight / lineHeight);
-
-        // appendLine
-        for (const line of textArray) {
-            if (line.startsWith("DELAY:")) {
-                const delayTime = parseFloat(line.substring(6));
-                await pause(delayTime * 1000);
-            } else {
-
-                // processLine
-                if (!line.includes("GRANTED")) {
-                    lines.push(line);
-                } else {
-                    break; // showAccessGranted could be here, but the indentation was building up
-                }
-                if (lines.length > maxLines) {
-                    lines.shift();
-                }
-                consoleElement.innerHTML = lines.join("<br>");
-                await pause(delay);
-            }
-        }
-
-        // showAccessGranted
-        await pause(1000);
-        document.querySelector('.access-granted').style.display = 'block';
-
-        // hideElements
-        await pause(3000);
-        document.getElementById("console").style.display = 'none';
-        document.getElementById("console").style.opacity = '0';
-        document.querySelector('.container').style.display = 'none';
-        document.querySelector('.access-granted').style.display = 'none';
-        document.getElementById("matrix").style.display = 'block';
-        await pause(1000);
-
-        // scrambleText
-        const element = matrixElement;
-        const duration = 5;
-        const speed = 100;
-        const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&';
-        const iterations = duration * (1000 / speed);
-        element.textContent = "111111111111";
-
-        // updateText
-        for (let currentIteration = 0; currentIteration < iterations; currentIteration++) {
-            let scramble = '';
-            for (let i = 0; i < element.textContent.length; i++) {
-                scramble += chars.charAt(Math.floor(Math.random() * chars.length));
-            }
-            element.textContent = scramble;
-            await pause(speed);
-        }
-
-        try {
-            const response = await fetchConfig; // fetch('config.json');
-            if (!response.ok) {
-                throw new Error('Failed to fetch questions');
-            }
-            scavengerHuntData = await response.json();
-        } catch (error) {
-            console.error('Error fetching questions:', error);
-        }
-
+    windowhash = window.location.hash.toLowerCase();
+    if(windowhash == "#misinformation") {
         section_number = 0;
-        question_number = 0;
-
-        windowhash = window.location.hash.toLowerCase();
-        if(windowhash == "#misinformation") {
-          section_number = 0;
-          autoCycleSections = false;
-        } else if(windowhash == "#onlinesafety") {
-          section_number = 1;
-          autoCycleSections = false;
-        } else if(windowhash == "#2fa") {
-          section_number = 2;
-          autoCycleSections = false;
-        } else if(windowhash == "#searchhistory") {
-          section_number = 3;
-          autoCycleSections = false;
-        } else if(windowhash == "#ai") {
-          section_number = 4;
-          autoCycleSections = false;
-        }
-
-        currq = scavengerHuntData.sections[section_number].questions[question_number];
-        element.textContent = currq.question;
-
-        await showQuestion_Prep(currq);
-
-        nextQuestion(section_number, question_number-1)
-        questionPopup(element, currq, section_number, question_number);
+        autoCycleSections = false;
+        document.querySelector("#subtitle-txt").innerText = "MISINFORMATION";
+    } else if(windowhash == "#onlinesafety") {
+        section_number = 1;
+        autoCycleSections = false;
+        document.querySelector("#subtitle-txt").innerText = "ONLINE SAFETY";
+    } else if(windowhash == "#2fa") {
+        section_number = 2;
+        autoCycleSections = false;
+        document.querySelector("#subtitle-txt").innerText = "2 FACTOR AUTHENTICATION";
+    } else if(windowhash == "#searchhistory") {
+        section_number = 3;
+        autoCycleSections = false;
+        document.querySelector("#subtitle-txt").innerText = "SEARCH HISTORY";
+    } else if(windowhash == "#ai") {
+        section_number = 4;
+        autoCycleSections = false;
+        document.querySelector("#subtitle-txt").innerText = "ARTIFICIAL INTELLIGENCE";
+    } else {
+        document.querySelector(".subtitle").style.display = "none";
     }
-});
+
+    // make sure section/questions are loaded before loading event listener
+
+    document.addEventListener('keydown', async function start(event) {
+        if (event.key === 'Enter') {
+            document.removeEventListener('keydown', start);
+    
+            // fadeOutElements
+            document.querySelector('.title').classList.add('fade-out');
+            document.querySelector('.subtitle').classList.add('fade-out');
+            document.querySelector('.begin-text').classList.add('fade-out');
+            await pause(2100);
+            document.getElementById("console").style.display = 'block';
+    
+            // fetchTextFromFile
+            let textArray = [];
+            try {
+                const response = await fetchText; // await fetch('consoleanimationtext.txt');
+                if (!response.ok) {
+                    throw new Error('Failed to fetch text');
+                }
+                const text = await response.text();
+                textArray = text.split('\n');
+            } catch (error) {
+                console.error('Error fetching text:', error);
+            }
+    
+            // displayConsoleOutput
+            const delay = 10;
+            const consoleElement = document.getElementById('console');
+            let lines = [];
+    
+            const viewportHeight = window.innerHeight;
+            const paddingTopBottom = 40 * 2;
+            const fontSize = parseFloat(getComputedStyle(consoleElement).fontSize);
+            const lineHeight = fontSize * 1.2;
+            const availableHeight = viewportHeight - paddingTopBottom;
+            const maxLines = Math.floor(availableHeight / lineHeight);
+    
+            // appendLine
+            for (const line of textArray) {
+                if (line.startsWith("DELAY:")) {
+                    const delayTime = parseFloat(line.substring(6));
+                    await pause(delayTime * 1000);
+                } else {
+    
+                    // processLine
+                    if (!line.includes("GRANTED")) {
+                        lines.push(line);
+                    } else {
+                        break; // showAccessGranted could be here, but the indentation was building up
+                    }
+                    if (lines.length > maxLines) {
+                        lines.shift();
+                    }
+                    consoleElement.innerHTML = lines.join("<br>");
+                    await pause(delay);
+                }
+            }
+    
+            // showAccessGranted
+            await pause(1000);
+            document.querySelector('.access-granted').style.display = 'block';
+    
+            // hideElements
+            await pause(3000);
+            document.getElementById("console").style.display = 'none';
+            document.getElementById("console").style.opacity = '0';
+            document.querySelector('.container').style.display = 'none';
+            document.querySelector('.access-granted').style.display = 'none';
+            document.getElementById("matrix").style.display = 'block';
+            await pause(1000);
+    
+            // scrambleText
+            const element = matrixElement;
+            const duration = 5;
+            const speed = 100;
+            const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&';
+            const iterations = duration * (1000 / speed);
+            element.textContent = "111111111111";
+    
+            // updateText
+            for (let currentIteration = 0; currentIteration < iterations; currentIteration++) {
+                let scramble = '';
+                for (let i = 0; i < element.textContent.length; i++) {
+                    scramble += chars.charAt(Math.floor(Math.random() * chars.length));
+                }
+                element.textContent = scramble;
+                await pause(speed);
+            }
+    
+            try {
+                const response = await fetchConfig; // fetch('config.json');
+                if (!response.ok) {
+                    throw new Error('Failed to fetch questions');
+                }
+                scavengerHuntData = await response.json();
+            } catch (error) {
+                console.error('Error fetching questions:', error);
+            }
+    
+            currq = scavengerHuntData.sections[section_number].questions[question_number];
+            element.textContent = currq.question;
+    
+            await showQuestion_Prep(currq);
+    
+            nextQuestion(section_number, question_number-1)
+            questionPopup(element, currq, section_number, question_number);
+        }
+    });
+})
 
 async function questionPopup(parent, q, snum, qnum) {
     const boxesparent = document.createElement("div");
